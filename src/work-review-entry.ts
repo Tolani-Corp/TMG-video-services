96d95173a8b548b0defa92aef5b0fdbe9f17a5b6
TolaniCorp6 <173643106+TolaniCorp@users.noreply.github.com>
2026-08-27 19:54:07 -0400

    Export processor chain workflow and media container

--- src/work-review-entry.ts
+++ src/work-review-entry.ts
@@ -1,4 +1,6 @@
 import reviewWorker from "./work-review";
 
 export { WorkRequestProcessingWorkflow } from "./work-request-processing-workflow";
+export { ProcessorChainWorkflow } from "./processor-chain-workflow";
+export { MediaExecutionContainer } from "./media-execution-container";
 export default reviewWorker;
